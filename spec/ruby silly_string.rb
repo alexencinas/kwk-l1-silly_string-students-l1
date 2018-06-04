b74fdd9283f2bd2a4422583fcc4295be23357204
alexencinas <shoogiee123@gmail.com>
2018-06-04 21:02:11 +0000

    automatically backed up by learn

--- spec/ruby silly_string.rb
+++ spec/ruby silly_string.rb
@@ -1,6 +1,1 @@
-puts "ecstatic".upcase
-puts "IM NOT SHOUTING".downcase
-puts "supercalifragilisticexpialidocious".length
-puts "supercalifragilisticexpialidocious".length + "wow" + "mom"
-puts "i really like programming".capitalize
-puts "Astounding aardvarks, arguably an ancient animal, always ate apples and acorns and artichokes all around Athens, amazing!"
+puts "ecstatic".capitalize
